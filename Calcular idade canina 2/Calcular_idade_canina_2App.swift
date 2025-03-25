884fce8e0ee8a522a0c5a7ba6751d4fbc3994687
Arthur Almeida Reis <159451483+arthuralmeidareis@users.noreply.github.com>
2025-03-25 09:02:28 -0300

    Add files via upload

---
+++ Calcular idade canina 2/Calcular_idade_canina_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Calcular_idade_canina_2App.swift
+//  Calcular idade canina 2
+//
+//  Created by COTEMIG on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Calcular_idade_canina_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
